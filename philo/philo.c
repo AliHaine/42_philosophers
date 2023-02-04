1942161a76675c6aeee6e6b715a66439b9f9661b
AliHaine <alitr57120@gmail.com>
2023-02-04 16:39:40 +0100

    update minor

--- philo/philo.c
+++ philo/philo.c
@@ -1,3 +1,24 @@
-//
-// Created by Ali Yagmur on 1/28/23.
-//
+#include "philo.h"
+
+void	ta_func(int *i)
+{
+	i = i + 1;
+	printf("ta %d\n", *i);
+}
+
+void	tb_func(int *i)
+{
+	i++;
+	printf("tb %d\n", *i);
+}
+
+int	main(int argc, char **argv)
+{
+	int i = 0;
+	pthread_t tA, tB;
+
+	pthread_create(&tA, NULL, ta_func, NULL);
+	pthread_create(&tB, NULL, tb_func, NULL);
+
+	return (0);
+}
---
+++ philo/philo.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayagmur <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/21 16:00:57 by ayagmur           #+#    #+#             */
+/*   Updated: 2023/01/27 17:00:15 by ayagmur          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <stdbool.h>
+# include <pthread.h>
+
+# define ERROR_ALR_NBR "Ce nombre est déjà défini"
+# define ERROR_NOT_NBR "Seul les nombres sont acceptes"
+
+typedef struct s_philo
+{
+	int 			id;
+	int				eated;
+	short			stats;
+	bool			fork;
+}					t_philo;
+
+typedef struct s_rules
+{
+	int				nbr_philo;
+	int				nbr_to_eat;
+}					t_rules;
+
+void				set_nn(struct s_pile *p1, int size, int i, int low);
+
+#endif
